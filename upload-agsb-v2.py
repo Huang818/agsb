f2e025cc7d19a6bf2250e047d55ce9f36b35ebcd
xiaohuang <huang690818@163.com>
2025-06-08 14:44:40 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "huang"         # 用户名
+UUID = "f90201f6-de66-4609-8754-9d01975ed6de"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "iu.xiaohuang001.ggff.net"                   # 域名，留空则自动获取
+CF_TOKEN = "{"AccountTag":"f6bafc63594aa099c6f60f751611c047","TunnelSecret":"Ap79C+2H91ma3HRuly8P+Tyzwr+Y2RYrYZxbZK7exZ8=","TunnelID":"15ad4084-6b2a-4aa4-a14c-ca009e7eb136","Endpoint":""}"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
